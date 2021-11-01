6fa8efc8f5dc652d095b8a61195f77519d4fd274
Santiago Perez <santy.p510@gmail.com>
2021-11-01 17:29:56 -0300

    Primer commit - inicio

---
+++ WorkMerge/src/main/java/com/WorkMerge/WorkMergeApplication.java
@@ -0,0 +1,13 @@
+package com.WorkMerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMergeApplication.class, args);
+	}
+
+}
---
+++ WorkMerge/src/test/java/com/WorkMerge/WorkMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WorkMerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
