ed109be8ade931337a381c10ffe4c7145319fb32
Francis Vaccarezza <francisvaccarezza@gmail.com>
2021-11-03 19:10:17 -0300

    Modificacion company,Service company hecho

--- WorkMerge/src/main/java/com/WorkMerge/services/CompanyService.java
+++ WorkMerge/src/main/java/com/WorkMerge/services/CompanyService.java
@@ -1,8 +1,17 @@
 package com.WorkMerge.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.WorkMerge.entities.Company;
+import com.WorkMerge.entities.Job;
+import com.WorkMerge.entities.Photo;
+import com.WorkMerge.enums.Rol;
 import com.WorkMerge.repositories.CompanyRepository;
 
 @Service
@@ -10,4 +19,64 @@ public class CompanyService {
 	
 	@Autowired
 	private CompanyRepository companyRepository;
+	//CREATE
+	@Transactional
+	public void newCompany(String email, String password, List<Job> job,Photo photo,MultipartFile archive)throws ErrorCompany {
+		Company company = new Company();
+		company.setRol(Rol.COMPANY);
+		company.setActive(true);
+		company.setEmail(email);
+		String encript = new BCryptPasswordEncoder().encode(password);
+		company.setPassword(encript);
+		company.setJob(job.newJob());
+		Photo photo = PhotoService.save(archive);
+	    company.setPhoto(photo);
+		companyRepository.save(company);
+	}
+	// UPDATE
+	@Transactional
+	public void updateCompany(String id,String password, List<Job> job,Photo photo,MultipartFile archive,String email) {
+		validate(email,password);
+		Optional<Company> compy = companyRepository.findById(id);
+		Company company = compy.get();
+		String encript = new BCryptPasswordEncoder().encode(password);
+		company.setPassword(encript);
+		company.setJob(job.newJob());
+		Photo photo = PhotoService.save(archive);
+	    company.setPhoto(photo);
+		companyRepository.save(company);
+	}
+	//DELETE
+	@Transactional
+	public void deleteCompany (String id,String password, List<Job> job,Photo photo,MultipartFile archive) throws ErrorCompany{
+		Optional<Company> compy = companyRepository.findById(id);
+		if (compy.isPresent()) {
+	            Company company = compy.get();
+	            companyRepository.delete(company);
+	        } else {
+	            throw new Error("No se encontro la compañía.");
+	        }
+		
+	}
+	//DOWNGRADE
+	@Transactional
+	public void downgradeCompany (String id,String password, List<Job> job,Photo photo,MultipartFile archive)throws ErrorCompany{
+		Optional<Company> compy = companyRepository.findById(id);
+		if (compy.isPresent()) {
+	            Company company = compy.get();
+	            company.setActive(false);
+	        } else {
+	            throw new Error("No se encontro la compañía.");
+	        }
+		
+	}
+	public void validate(String email,String password) throws ErrorCompany {
+		if(email==null || email.isEmpty() || email.equals("")) {
+			throw new ErrorCompany ("El email no puede ser nulo/vacío.");
+		}
+		if(password==null || password.isEmpty()||password.equals("")) {
+			throw new ErrorCompany ("La contraseña no puede ser nula/vacía o 0.");
+		}
+
+	}
 }
