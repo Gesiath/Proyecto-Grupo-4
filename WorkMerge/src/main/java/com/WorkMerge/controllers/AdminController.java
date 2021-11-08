4fea30130af99a04e50ecf2a420468d90aa79cbb
Maxi Mongelos <maximongelos@gmail.com>
2021-11-08 18:02:14 -0300

    Modificaciones

--- WorkMerge/src/main/java/com/WorkMerge/controllers/AdminController.java
+++ WorkMerge/src/main/java/com/WorkMerge/controllers/AdminController.java
@@ -32,12 +32,12 @@ public class AdminController {
 	}
 	
 	@GetMapping("/registro")
-	public String registerAdmin() {
+	public String crearAdmin() {
 		return this.viewPath.concat("register-admin");
 	}
 	
-	@PostMapping("/registro")
-	public String createAdmin(@RequestParam("email") String email, @RequestParam("nickname") String nickname, @RequestParam("password") String password) {
+	@PostMapping("/guardar")
+	public String guardarAdmin(@RequestParam("email") String email, @RequestParam("nickname") String nickname, @RequestParam("password") String password) {
 		try {
 			adminService.newAdmin(email, password, nickname);
 			return "redirect:/admin";
